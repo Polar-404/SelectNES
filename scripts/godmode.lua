d8d0ca755ad19bb4f412f714c7b79df06392f80d
Polaro4 <miguelraunh@gmail.com>
2026-07-03 12:28:11 -0300

    a few lua scripts as examples

---
+++ scripts/godmode.lua
@@ -0,0 +1,19 @@
+-- smb_godmode.lua
+
+function on_init()
+    print("=== SMB Godmode ===")
+end
+
+function on_frame()
+    -- gives invencibility
+    write_mem(0x079E, 0x05)
+
+    -- maximum lifes
+    write_mem(0x075A, 0x63)
+
+    -- forces fire mario
+    local estado_atual = read_mem(0x0754)
+    if estado_atual < 2 then
+        write_mem(0x0754, 2)
+    end
+end
